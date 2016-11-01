530075bed1ec46e81d3080e88306a7a69b0db84e
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-01 00:11:55 -0500

    Set up server.js

--- server.js
+++ server.js
@@ -1,3 +1,45 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+
+
+var app = express();
+var PORT = 3000;
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//REQUIREMENTS HERE
+
+//--------------------------------------------------------------------------------------------
+
+
+
+//Call server
+app.listen(PORT, function(){
+
+	console.log('App listening on PORT' + PORT);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
