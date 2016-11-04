a6fad6ddaf0093c8e62a0b6821f8d78c8c1cb04b
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-04 02:49:12 -0500

    pushing values

--- server.js
+++ server.js
@@ -15,31 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-
+app.use(express.static('public'));
 
 //--------------------------------------------------------------------------------------------
 
 //GET ROUTING
 
-app.get('/friends', function(req, res){
-
-	var friend = req.params.friends;
-	console.log(res);
-
-	if(friend){
-		for(var i=0; i < friends.length; i++){
-			if(friend === friends[i].name){
-				res.json(friends[i]);
-				return;
-			}
-		}
-
-		res.json(false);
-	} else {
-		res.json(friends);
-	}
-
-});
 
 require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
@@ -48,36 +29,6 @@ require("./app/routing/html-routes.js")(app);
 
 // require("./app/routing/html-routes.js");
 // require("./app/routing/api-routes.js");
-
-
-//--------------------------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------------------------
-
-//POST ROUTING
-
-app.post('/survey', function(req, res){
-	var user = {
-		"name": req.body.name,
-		"photo": req.body.image.Link,
-		"score": [
-			req.body.question1.value,
-			req.body.question2.value,
-			req.body.question3.value,
-			req.body.question4.value,
-			req.body.question5.value,
-			req.body.question6.value,
-			req.body.question7.value,
-			req.body.question8.value,
-			req.body.question9.value,
-			req.body.question10.value,
-		]
-	}
-	console.log("User: " + user);
-  	res.end("yes");
-
-
-});
 
 
 //--------------------------------------------------------------------------------------------
